ec7a125387838e4c31863ae2c52128def3207cc9
Eric de Ruiter <eric@thisisdevelopment.nl>
2022-12-11 13:24:07 +0100

    day 6

---
+++ 6b/solve.php
@@ -0,0 +1,20 @@
+<?php
+
+$data = file_get_contents('input.txt');
+$length = strlen($data);
+
+$buf = [];
+for ($i=0; $i<$length; $i++) {
+    $char = substr($data, $i, 1);
+    $found = array_search($char, $buf);
+    if ($found !== false) {
+        array_splice($buf, 0, $found+1, []);
+    }
+    $buf[] = $char;
+    if (count($buf) == 14) {
+        $i++;
+        $marker = implode('', $buf);
+        echo "{$marker} => {$i}\n";
+        break;
+    }
+}
